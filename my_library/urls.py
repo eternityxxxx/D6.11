7485e2afca9044a9b9cd2d1ab4137be053138cea
eternityxxxx <gpavlikov7@gmail.com>
2020-09-15 22:19:14 +0600

    Change urls.py

--- my_library/urls.py
+++ my_library/urls.py
@@ -15,21 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from p_library import views
+from p_library.views import BookTablePage, book_decrement, book_increment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', BookTablePage.as_view()),
 
-    path('books/', views.books_list),
-    path('authors/', views.authors_list),
-    path('publishers/', views.publishers_list),
-    path('friends/', views.friends_list),
-
-    path('books_table/', views.books_table),
     path('index/book_increment/', views.book_increment),
     path('index/book_decrement/', views.book_decrement),
-
-    path('author/create_many', views.author_create_many, name='author_create_many'),
-    path('book/create_many', views.book_create_many, name='book_create_many'),
 ]
