5d5239491c65baa4bc0412df027362bb8a8a1315
eternityxxxx <gpavlikov7@gmail.com>
2020-09-15 22:31:29 +0600

    Add static and media at url.py

--- my_library/urls.py
+++ my_library/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 from p_library.views import BookTablePage, book_decrement, book_increment
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BookTablePage.as_view()),
 
-    path('index/book_increment/', views.book_increment),
-    path('index/book_decrement/', views.book_decrement),
+    path('index/book_increment/', book_increment),
+    path('index/book_decrement/', book_decrement),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
